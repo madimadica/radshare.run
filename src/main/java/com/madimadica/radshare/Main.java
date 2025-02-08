32a0e78f34b7bdc3c347e0d7b0c64ac6cf796d48
Madi <145222821+madimadica@users.noreply.github.com>
2025-02-08 11:18:28 -0600

    Add Spring boilerplate setup

--- src/main/java/com/madimadica/radshare/Main.java
+++ src/main/java/com/madimadica/radshare/Main.java
@@ -1,7 +1,21 @@
 package com.madimadica.radshare;
 
-public class Main {
+import com.madimadica.jdbc.web.InitializeJdbc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
+
+@Import(InitializeJdbc.class)
+@SpringBootApplication
+public class Main extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Main.class);
     }
 }
