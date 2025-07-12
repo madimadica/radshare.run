140f382ba24d9cf845c2d4721a469f0193b66161
Madi Wilder <mattsmathclassofficial@gmail.com>
2025-07-12 17:05:33 -0500

    Update /api/v1/auth/me to /api/v1/account/me

--- src/main/java/com/madimadica/voidrelics/account/UserAccountController.java
+++ src/main/java/com/madimadica/voidrelics/account/UserAccountController.java
@@ -1,7 +1,11 @@
 package com.madimadica.voidrelics.account;
 
+import com.madimadica.voidrelics.auth.CustomUser;
+import com.madimadica.voidrelics.auth.dto.AuthenticatedUserDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/account")
 public class UserAccountController {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserAccountController.class);
+
+    @GetMapping("/me")
+    public ResponseEntity<AuthenticatedUserDto> me(CustomUser user) {
+        return ResponseEntity.ok(AuthenticatedUserDto.of(user));
+    }
 
 }
